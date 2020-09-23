0517972f334bd8b880bb991e8a5716f315433c60
Lorenalgm <lorena@proesc.com>
2020-09-23 13:34:37 -0300

    Criada index do formulário

--- src/pages/Invite/index.js
+++ src/pages/Invite/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 export default function Invite(){
     return(
-        <p>Oiiii</p>
+        <div className="container">
+            <div className="form">
+                <h1>Junte-se a Lorena GM no NLW</h1>
+                <p>Um evento online e gratuito para evoluirmos juntos para o próximo nível em programação. <a href="https://nextlevelweek.com/convite/lorenalgm">Saiba mais.</a></p>
+
+                <input type="text" placeholder="Nome"></input>
+                <input type="email" placeholder="E-mail"></input>
+                <a href="https://nextlevelweek.com/convite/lorenalgm" className="button">QUERO PARTICIPAR</a>
+            </div>
+            <div className="invite">
+                <div className="invite invite-secondary"></div>
+                <div className="invite"></div>
+                <div className="invite invite-secondary"></div>
+            </div>
+        </div>
     )
 }
